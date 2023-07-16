be17df649163baf0e67dac8934ddab1e96d08e76
mohamed <mohamed.ramadan19502@gmail.com>
2023-07-16 03:02:39 +0300

    My functions

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -7,8 +7,20 @@ class GetCityHasCheapestInternetConnectionInteractor(
 
 ) {
 
-    fun execute(): CityEntity{
-        throw Throwable(" Implemented yet")
+    fun execute(limit: Int): CityEntity? {
+        return dataSource
+            .getAllCitiesData()
+            .filter (::excludeNullSalariesAndNullInternetPrice)
+            .take(limit)
+            .minByOrNull (::InternetPriceVsAverageSalary)
     }
 
+    private fun excludeNullSalariesAndNullInternetPrice(cityEntity: CityEntity): Boolean {
+        return  cityEntity.averageMonthlyNetSalaryAfterTax != null
+                && cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null
+    }
+
+    private fun InternetPriceVsAverageSalary(cityEntity: CityEntity): Float {
+        return (cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl?.div(cityEntity.averageMonthlyNetSalaryAfterTax!!))!! * 100
+    }
 }
--- src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -12,7 +12,7 @@ class GetCityHasCheapestInternetConnectionInteractorTest {
 
     @BeforeAll
     fun setup() {
-        cheapestInternet = GetCityHasCheapestInternetConnectionInteractor()
+
     }
 
 
